6758f60aeb75246bcd9a209aa960da771ea543f4
lucavgobbi <gobbi@me.com>
2020-07-20 21:19:41 -0400

    MESSAGE

---
+++ sync.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MESSAGE=$1
+
+brew list -1 | awk '{ print "brew install" $0}' > ~/brew/brew.txt
+brew cask list -1 | awk '{ print "brew cask install " $0}' > ~/brew/cask.txt
+cp -f ~/.zshrc ~/brew/.zshrc
+git -C ~/brew add .
+
+git -C ~/brew commit -m MESSAGE
