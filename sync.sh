7664a227cf366c5998112269a07a9a76845d08be
lucavgobbi <gobbi@me.com>
2020-07-20 21:22:54 -0400

    Testing script

--- sync.sh
+++ sync.sh
@@ -6,5 +6,5 @@ brew list -1 | awk '{ print "brew install" $0}' > ~/brew/brew.txt
 brew cask list -1 | awk '{ print "brew cask install " $0}' > ~/brew/cask.txt
 cp -f ~/.zshrc ~/brew/.zshrc
 git -C ~/brew add .
-git -C ~/brew commit -m MESSAGE
-echo MESSAGE
+git -C ~/brew commit -m "$MESSAGE"
+echo "$MESSAGE"
