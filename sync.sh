5f8d371bd2e9d73db42319e1538e294ea0adb0f4
lucavgobbi <gobbi@me.com>
2020-11-05 08:58:16 -0500

    Updating script

--- sync.sh
+++ sync.sh
@@ -3,8 +3,8 @@
 MESSAGE=$1
 
 brew tap | awk '{ print "brew tap " $0}' > ~/mysetup/tap.txt
-brew list -1 | awk '{ print "brew install " $0}' > ~/mysetup/brew.txt
-brew cask list -1 | awk '{ print "brew cask install " $0}' > ~/mysetup/cask.txt
+brew list --forula -1 | awk '{ print "brew install " $0}' > ~/mysetup/brew.txt
+brew cask list --cask -1 | awk '{ print "brew cask install " $0}' > ~/mysetup/cask.txt
 cp -f ~/.zshrc ~/mysetup/.zshrc
 git -C ~/mysetup add .
 git -C ~/mysetup commit -m "$MESSAGE"
